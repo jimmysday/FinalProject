7eabd649c2846ff357b0aeb9fea16c5083a36155
jimmysday <jaminzhang99@gmail.com>
2025-03-31 17:41:00 -0700

    design the building

--- Assets/Scripts/MainDoorContrall.cs
+++ Assets/Scripts/MainDoorContrall.cs
@@ -4,12 +4,17 @@ public class MainDoorContrall : MonoBehaviour
 {
     private bool doorIsOpen = false;
     private Vector3 homePos;
-    private Vector3 closeOffset = new Vector3(-2, 0, 0);
+    private Vector3 closeOffset = new Vector3(-1.5f, 0, 0);
+
+    //private Quaternion closedRotation;
+    //private Quaternion openRotation;
     private float moveTime = 2.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         homePos = transform.position;
+        //closedRotation = transform.rotation;
+        //openRotation = Quaternion.Euler(0, 90, 0) * closedRotation; // Rotate door 90 degrees on Y-axis
     }
 
     // Update is called once per frame
@@ -20,13 +25,26 @@ public class MainDoorContrall : MonoBehaviour
 
     public void Operate()
     {
-        Debug.Log("trigger door");
+        //if (doorIsOpen)
+        //{
+        //    Debug.Log("Closing door");
+        //    iTween.RotateTo(this.gameObject, closedRotation.eulerAngles, moveTime);
+        //}
+        //else
+        //{
+        //    Debug.Log("Opening door");
+        //    iTween.RotateTo(this.gameObject, openRotation.eulerAngles, moveTime);
+        //}
+        //doorIsOpen = !doorIsOpen;
+
         if (doorIsOpen)
         {
+            Debug.Log("close door");
             iTween.MoveTo(this.gameObject, homePos, moveTime);
         }
         else
         {
+            Debug.Log("open door");
             iTween.MoveTo(this.gameObject, homePos + closeOffset, moveTime);
         }
         doorIsOpen = !doorIsOpen;
