262c347f02292f202ca85ae5b9b2879d864df387
jimmysday <jaminzhang99@gmail.com>
2025-04-03 12:50:51 -0700

    adding animation for soldier

--- Assets/Scripts/SoldierController.cs
+++ Assets/Scripts/SoldierController.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class SoldierController : MonoBehaviour
 {
+    public float IdleTime { get; private set; } = 3.0f;         // time to spend in idle state
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
