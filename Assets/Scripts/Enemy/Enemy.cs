1dd6fcb201f1f1cac61295dbe79e543829f9e7a7
jimmysday <jaminzhang99@gmail.com>
2025-04-19 08:01:34 -0700

    added dead animation of archor

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -5,7 +5,7 @@ using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
-    public float IdleTime { get; private set; } = 3.0f;         // time to spend in idle state
+    public float IdleTime { get; private set; } = 2.0f;         // time to spend in idle state
     public float ChaseRange { get; private set; } = 10.0f;      // when player is closer than this, chase
     public float AttackRange { get; private set; } = 5.0f;      // when player is closer than this, attack
     public float AttackRangeStop { get; private set; } = 7.0f; // when player is farther than this, chase
@@ -71,5 +71,9 @@ public class Enemy : MonoBehaviour
         //projectile.transform.Rotate(0, 90, 0); // Adjust value to correct the direction
         Rigidbody rb = projectile.GetComponent<Rigidbody>();
         rb.AddForce(-transform.right * projectileForce, ForceMode.Impulse);
+    }
+    private void DeadEvent()
+    {
+        Destroy(this.gameObject);
     }
 }
